afed990681eeda7e841a0971127dbc3e9d76be89
RuanJG <ruan.mails@gmail.com>
2015-09-23 19:08:00 +0800

    Ruan: add cali rc func, and fix to arduino pro micro

--- rc_Jostick/rc_Jostick.ino
+++ rc_Jostick/rc_Jostick.ino
@@ -5,6 +5,13 @@
 #include <mavlink.h>
 
 
+
+#define BOARD_PRO_MICRO 1
+#define BOARD_UNO 2
+//############################# change this for you board
+#define MY_BOARD_TYPE BOARD_PRO_MICRO
+//#############################
+
 //main loop
 #define DELAY_TIME 20 //ms    1000/DELAY_TIME =  rate
 #define MAVLINK_CONNECT_TIMEOUT 150 // 3s: 3000/DELAY_TIME
@@ -36,20 +43,32 @@
 #define CHAN_RC_MODE_6_VALUE   1800
 
 #define CHAN_RC_MIN_VALUE 1000
+#define CHAN_RC_MAX_VALUE 2000
 #define CHAN_RC_SCALE(x) x+ CHAN_RC_MIN_VALUE
 
-int chan_rc_value[8]={ 0,0,0,0, CHAN_RC_MODE_1_VALUE ,0,0,0 };
-uint8_t chan_rc_pin_type[8]={ CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE, CHAN_MODE_TYPE,  CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE, CHAN_GPIO_TYPE };
-int chan_rc_pin[8]= { A0,A1,A2,A3, 0 ,A4,A5,8}; // roll pitch,thr,roll, adc key 1, adc key 2, gpio 4, gpio 5
-int chan_rc_sensor_max_min_value[8][2]={
-  {390,640},
-  {390,640},
-  {0,911},
-  {330,680},
-  {0,1023}, // mode type , no sense
-  {0,1023},
-  {0,1023},
-  {0,1000} //gpio
+int need_cali_rc = 0;
+int chan_rc_value[8]={ 0,0,0,0, CHAN_RC_MODE_1_VALUE ,CHAN_RC_MODE_1_VALUE,CHAN_RC_MODE_1_VALUE,CHAN_RC_MODE_1_VALUE };//0,0,0 };
+uint8_t chan_rc_pin_type[8]={ CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE ,CHAN_ANALOG_TYPE, CHAN_MODE_TYPE,  CHAN_MODE_TYPE ,CHAN_MODE_TYPE, CHAN_MODE_TYPE };//CHAN_GPIO_TYPE ,CHAN_GPIO_TYPE, CHAN_GPIO_TYPE };
+int chan_rc_pin[8]= { A0,A1,A2,A3, 0 ,8,8,8}; // roll pitch,thr,roll, adc key 1, adc key 2, gpio 4, gpio 5
+int chan_rc_sensor_max_min_value[8][3]={
+  {240, 0 ,845},
+  {170, 0 ,780},
+  {45, 0 ,980},
+  {0, 0 ,760},//1016},
+  {0, 0 ,1023}, // mode type , no sense
+  {0, 0 ,1000},//gpio
+  {0, 0 ,1000},//gpio
+  {0, 0 ,1000} //gpio
+  };
+int chan_rc_min_max_tmp[8][2]={
+  {0,1},
+  {0,1},
+  {0,1},
+  {0,1},//1016},
+  {0,1}, // mode type , no sense
+  {0,1},//gpio
+  {0,1},//gpio
+  {0,1} //gpio
   };
 void setup_chan_pin_type()
 {
@@ -69,7 +88,7 @@ typedef enum KEY_FUNCTION_ID_TT
   FUNC_SWITCH_MODE,
   KEY_MAX_COUNT
 } KEY_FUNCTION_ID;
-int key_pin[KEY_MAX_COUNT] = {9, 10, 8};
+int key_pin[KEY_MAX_COUNT] = {3, 4, 5};
 uint8_t key_value[KEY_MAX_COUNT]={0,0,0};
 uint8_t key_function_status[KEY_MAX_COUNT]={0 , 0, 0};//the status changed after triggle happen
 
@@ -134,7 +153,80 @@ uint8_t ready_for_send_rc = 0;
 
 int scale_rc_value(int id, int val)
 {
-  return map( val, chan_rc_sensor_max_min_value[id][0], chan_rc_sensor_max_min_value[id][1], 1000, 2000);
+  if( DEBUG_RC == 1 )
+      return val;
+  else
+      return map( val, chan_rc_sensor_max_min_value[id][0], chan_rc_sensor_max_min_value[id][2], 1000, 2000);
+}
+void update_rc_trim_value()
+{
+   int t,i,j,id ,val;
+   int times = 30;
+   int chan_count  = 3;
+   int ids[3]={ROLL_ID, PITCH_ID, YAW_ID};
+   int sum[3]={0,0,0};
+   int sum_val;
+ 
+   for ( i = 0; i< times; i++ )
+   {
+       for( j = 0; j< chan_count; j++)
+       {
+           id = ids[j];
+           val = analogRead( chan_rc_pin[ id ] );
+           sum[j] += val;
+       }
+       delay(50);
+   }
+
+   for ( i = 0 ; i< chan_count; i++)
+   {
+          sum[i] = sum[i]/times;
+          id = ids[i];
+          chan_rc_sensor_max_min_value[id][1] = sum[i];
+          chan_rc_min_max_tmp[id][0] = chan_rc_sensor_max_min_value[id][1]- 10;
+          chan_rc_min_max_tmp[id][1] = chan_rc_sensor_max_min_value[id][1]+ 10;
+   }
+   
+   need_cali_rc =1;
+}
+void update_rc_min_max_vaule(int id)
+{
+    int max_to_trim ;
+    int min_to_trim;
+    int rang;
+    
+    max_to_trim = chan_rc_min_max_tmp[id][1] - chan_rc_sensor_max_min_value[id][1];
+    min_to_trim = chan_rc_sensor_max_min_value[id][1] - chan_rc_min_max_tmp[id][0];
+    
+    if( max_to_trim < min_to_trim ){
+      chan_rc_sensor_max_min_value[id][0] = chan_rc_sensor_max_min_value[id][1] - max_to_trim;
+      chan_rc_sensor_max_min_value[id][2] = chan_rc_sensor_max_min_value[id][1] + max_to_trim;
+    }else{
+      chan_rc_sensor_max_min_value[id][0] = chan_rc_sensor_max_min_value[id][1] - min_to_trim;
+      chan_rc_sensor_max_min_value[id][2] = chan_rc_sensor_max_min_value[id][1] + min_to_trim;
+    }
+    Serial.print("id=");
+    Serial.print(id);
+    Serial.print("trim=");
+    Serial.print(chan_rc_sensor_max_min_value[id][1]);
+    Serial.print(", max=");
+    Serial.print(chan_rc_sensor_max_min_value[id][2]);
+        Serial.print(", min=");
+        Serial.print(chan_rc_sensor_max_min_value[id][0]);
+    Serial.println(" ");
+      
+}
+void adc_sensor_cali(int id, int val)
+{
+  int i;
+  //recovery the min or max value
+  if( val < chan_rc_min_max_tmp[id][0] ) {
+    chan_rc_min_max_tmp[id][0] = val;
+    update_rc_min_max_vaule(id);
+  }else if ( val > chan_rc_min_max_tmp[id][1] ){
+    chan_rc_min_max_tmp[id][1] = val;
+    update_rc_min_max_vaule(id);
+  }
 }
 int get_rc_pin_value(int id)
 {
@@ -146,6 +238,8 @@ int get_rc_pin_value(int id)
       val = chan_rc_value[id];
     }else{
        val = analogRead(chan_rc_pin[id]);
+         if( 1 == need_cali_rc && id != THR_ID)
+             adc_sensor_cali(id,val);
     }
 /*
       Serial.print("chan ");
@@ -170,8 +264,10 @@ void update_chan_rc_value()
   for( i = 0; i< CHAN_COUNT; i++)
   {
     chan_rc_value[i] = get_rc_pin_value(i);
-    if( chan_rc_value[i] < CHAN_RC_MIN_VALUE && CHAN_MODE_TYPE != chan_rc_pin_type[i] )
+    if( DEBUG_RC != 1 &&   chan_rc_value[i] < CHAN_RC_MIN_VALUE &&    CHAN_MODE_TYPE != chan_rc_pin_type[i] )
       chan_rc_value[i] = CHAN_RC_MIN_VALUE;
+    if( DEBUG_RC != 1 &&   chan_rc_value[i] > CHAN_RC_MAX_VALUE &&    CHAN_MODE_TYPE != chan_rc_pin_type[i] )
+      chan_rc_value[i] = CHAN_RC_MAX_VALUE;
 #if DEBUG_RC
   Serial.print(chan_rc_value[i]);
   Serial.print(",");
@@ -256,13 +352,43 @@ void update_mavlink_status()
   }
   
 }
+int do_write_uart(uint8_t *buf, int len)
+{
+  int ret;
+   if( MY_BOARD_TYPE == BOARD_PRO_MICRO ){
+      ret = Serial1.write(buf,len);
+   }else{
+      ret = Serial.write(buf,len);
+   } 
+   return ret;
+}
+uint8_t do_read_uart()
+{
+  uint8_t ret;
+   if( MY_BOARD_TYPE == BOARD_PRO_MICRO ){
+      ret = Serial1.read();
+   }else{
+      ret = Serial.read();
+   } 
+   return ret;
+}
+int is_uart_available()
+{
+  int ret;
+   if( MY_BOARD_TYPE == BOARD_PRO_MICRO ){
+      ret = Serial1.available();
+   }else{
+      ret = Serial.available();
+   } 
+   return ret;
+}
 int send_mavlink_message(mavlink_message_t *message)
 {
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
   uint16_t len = mavlink_msg_to_send_buffer(buf, message);
   int ret;
 
-  ret = Serial.write(buf, len);
+  ret = do_write_uart(buf, len);
   ret= ret==len ? 1:0;
 #if DEBUG_MAVLINK
   if( ret == 0 )
@@ -364,8 +490,8 @@ void receive_and_handleMessage() {
   int recived = 0;
   
   //receive data over serial 
-  while(Serial.available() > 0) { 
-    uint8_t c = Serial.read();
+  while(is_uart_available() > 0) { 
+    uint8_t c = do_read_uart();
     if( mavlink_parse_char(MAVLINK_COMM_0, c, &msg, &status) ) { 
       // Handle message
       recived = 1;
@@ -407,7 +533,7 @@ void receive_and_handleMessage() {
 void mavlink_msg_loop() { 
   //if( 1 == ready_for_send_rc )
     send_rc_override_messages();
-  receive_and_handleMessage();
+  //receive_and_handleMessage();
 }
 
 
@@ -509,14 +635,17 @@ void triggle_led(int id)
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(57600);
+  if( MY_BOARD_TYPE == BOARD_PRO_MICRO )
+    Serial1.begin(57600);
   setup_chan_pin_type();
   setup_key_pin_mode();
   setup_led_pin_mode();
+  update_rc_trim_value();
 }
 
-void loop() {
+void loop() {    
   update_rc_loop();
-  update_key_loop();
+  //update_key_loop();
   mavlink_msg_loop();
   
   delay(DELAY_TIME);
